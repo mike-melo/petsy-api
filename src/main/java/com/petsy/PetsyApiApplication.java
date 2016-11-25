f95614d11cb3fd448e017edbc65b779fc2df82a4
Michael Melo <melom@intelliware.ca>
2016-11-25 10:33:32 -0500

    Spring initializr seed

---
+++ src/main/java/com/petsy/PetsyApiApplication.java
@@ -0,0 +1,12 @@
+package com.petsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsyApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/petsy/PetsyApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.petsy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetsyApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
