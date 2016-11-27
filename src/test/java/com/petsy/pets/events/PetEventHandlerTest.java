2e189b022a80e6e75f8e43d9c891bbb4d78ae1cd
Michael Melo <melom@intelliware.ca>
2016-11-27 11:30:52 -0500

    Tests for PetEventHandler

--- src/test/java/com/petsy/pets/events/PetEventHandlerTest.java
+++ src/test/java/com/petsy/pets/events/PetEventHandlerTest.java
@@ -1,7 +1,42 @@
 package com.petsy.pets.events;
 
-/**
- * Created by melom on 27/11/2016.
- */
+import com.petsy.exceptions.ValidationException;
+import com.petsy.pets.domain.Pet;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.fail;
+
+@Test
 public class PetEventHandlerTest {
+
+    private PetEventHandler fixture;
+
+    @Mock
+    private Pet pet;
+
+    @BeforeMethod
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        fixture = new PetEventHandler();
+    }
+
+    @Test(expectedExceptions = ValidationException.class)
+    public void invalidPet_handleBeforeCreate_shouldThrowAValidationException() {
+        when(pet.isValid()).thenReturn(false);
+        fixture.handleBeforeCreate(pet);
+    }
+
+    public void validPet_handleBeforeCreate_shouldNotThrowAnException() {
+        when(pet.isValid()).thenReturn(true);
+        try {
+            fixture.handleBeforeCreate(pet);
+        } catch (ValidationException e) {
+            fail("A ValidationException should not have been thrown");
+        }
+    }
+
 }
